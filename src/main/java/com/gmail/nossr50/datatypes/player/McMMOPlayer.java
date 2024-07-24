49aa4b2d03c234541d04d7767710efe550b1c653
chengfeng30121 <122871550+chengfeng30121@users.noreply.github.com>
2024-07-24 10:50:07 +0000

    deleted:    .github/workflows/maven.yml modified:   Changelog.txt modified:   pom.xml renamed:    src/main/java/com/gmail/nossr50/commands/player/McrankCommand.java -> src/main/java/com/gmail/nossr50/commands/player/McRankCommand.java renamed:    src/main/java/com/gmail/nossr50/commands/player/MctopCommand.java -> src/main/java/com/gmail/nossr50/commands/player/McTopCommand.java modified:   src/main/java/com/gmail/nossr50/commands/skills/MacesCommand.java modified:   src/main/java/com/gmail/nossr50/commands/skills/MiningCommand.java modified:   src/main/java/com/gmail/nossr50/config/AdvancedConfig.java modified:   src/main/java/com/gmail/nossr50/config/GeneralConfig.java modified:   src/main/java/com/gmail/nossr50/config/SoundConfig.java modified:   src/main/java/com/gmail/nossr50/database/SQLDatabaseManager.java modified:   src/main/java/com/gmail/nossr50/datatypes/player/McMMOPlayer.java modified:   src/main/java/com/gmail/nossr50/datatypes/player/PlayerProfile.java modified:   src/main/java/com/gmail/nossr50/datatypes/skills/SubSkillType.java modified:   src/main/java/com/gmail/nossr50/datatypes/skills/subskills/acrobatics/Roll.java modified:   src/main/java/com/gmail/nossr50/listeners/EntityListener.java modified:   src/main/java/com/gmail/nossr50/mcMMO.java modified:   src/main/java/com/gmail/nossr50/party/PartyManager.java modified:   src/main/java/com/gmail/nossr50/runnables/commands/McRankCommandAsyncTask.java renamed:    src/main/java/com/gmail/nossr50/runnables/commands/McrankCommandDisplayTask.java -> src/main/java/com/gmail/nossr50/runnables/commands/McRankCommandDisplayTask.java modified:   src/main/java/com/gmail/nossr50/runnables/skills/AlchemyBrewTask.java modified:   src/main/java/com/gmail/nossr50/skills/alchemy/AlchemyPotionBrewer.java modified:   src/main/java/com/gmail/nossr50/skills/axes/AxesManager.java modified:   src/main/java/com/gmail/nossr50/skills/maces/MacesManager.java modified:   src/main/java/com/gmail/nossr50/skills/mining/BlastMining.java modified:   src/main/java/com/gmail/nossr50/skills/mining/MiningManager.java modified:   src/main/java/com/gmail/nossr50/skills/swords/SwordsManager.java modified:   src/main/java/com/gmail/nossr50/skills/unarmed/UnarmedManager.java modified:   src/main/java/com/gmail/nossr50/util/EnchantmentMapper.java modified:   src/main/java/com/gmail/nossr50/util/ItemUtils.java modified:   src/main/java/com/gmail/nossr50/util/commands/CommandRegistrationManager.java modified:   src/main/java/com/gmail/nossr50/util/commands/CommandUtils.java modified:   src/main/java/com/gmail/nossr50/util/platform/PlatformBuilder.java modified:   src/main/java/com/gmail/nossr50/util/platform/PlatformManager.java modified:   src/main/java/com/gmail/nossr50/util/random/Probability.java modified:   src/main/java/com/gmail/nossr50/util/random/ProbabilityUtil.java modified:   src/main/java/com/gmail/nossr50/util/skills/CombatUtils.java modified:   src/main/java/com/gmail/nossr50/util/skills/ParticleEffectUtils.java modified:   src/main/java/com/gmail/nossr50/util/skills/SkillTools.java modified:   src/main/java/com/gmail/nossr50/util/sounds/SoundManager.java modified:   src/main/java/com/gmail/nossr50/util/sounds/SoundType.java modified:   src/main/resources/advanced.yml modified:   src/main/resources/config.yml modified:   src/main/resources/experience.yml modified:   src/main/resources/locale/locale_en_US.properties modified:   src/main/resources/locale/locale_fr.properties modified:   src/main/resources/locale/locale_pt_BR.properties modified:   src/main/resources/locale/locale_zh_CN.properties modified:   src/main/resources/locale/locale_zh_TW.properties modified:   src/main/resources/plugin.yml modified:   src/main/resources/repair.vanilla.yml modified:   src/main/resources/skillranks.yml modified:   src/main/resources/sounds.yml modified:   src/test/java/com/gmail/nossr50/MMOTestEnvironment.java modified:   src/test/java/com/gmail/nossr50/skills/woodcutting/WoodcuttingTest.java

--- src/main/java/com/gmail/nossr50/datatypes/player/McMMOPlayer.java
+++ src/main/java/com/gmail/nossr50/datatypes/player/McMMOPlayer.java
@@ -2,6 +2,7 @@ package com.gmail.nossr50.datatypes.player;
 
 import com.gmail.nossr50.api.exceptions.InvalidSkillException;
 import com.gmail.nossr50.chat.author.PlayerAuthor;
+import com.gmail.nossr50.config.AdvancedConfig;
 import com.gmail.nossr50.config.ChatConfig;
 import com.gmail.nossr50.config.WorldBlacklist;
 import com.gmail.nossr50.config.experience.ExperienceConfig;
@@ -112,7 +113,6 @@ public class McMMOPlayer implements Identified {
     private int respawnATS;
     private int teleportATS;
     private long databaseATS;
-    private double attackStrength; //captured during arm swing events
     //private int chimeraWingLastUse;
     private Location teleportCommence;
 
@@ -151,7 +151,6 @@ public class McMMOPlayer implements Identified {
         experienceBarManager = new ExperienceBarManager(this);
 
         debugMode = false; //Debug mode helps solve support issues, players can toggle it on or off
-        attackStrength = 1.0D;
 
         this.playerAuthor = new PlayerAuthor(player);
 
@@ -227,7 +226,9 @@ public class McMMOPlayer implements Identified {
                 skillManagers.put(primarySkillType, new WoodcuttingManager(this));
                 break;
             case MACES:
-                skillManagers.put(primarySkillType, new MacesManager(this));
+                if (mcMMO.getCompatibilityManager().getMinecraftGameVersion().isAtLeast(1, 21, 0)) {
+                    skillManagers.put(primarySkillType, new MacesManager(this));
+                }
                 break;
             default:
                 throw new InvalidSkillException("The skill named has no manager! Contact the devs!");
@@ -239,7 +240,11 @@ public class McMMOPlayer implements Identified {
     }
 
     public double getAttackStrength() {
-        return attackStrength;
+        if (mcMMO.p.getAdvancedConfig().useAttackCooldown()) {
+            return player.getAttackCooldown();
+        } else {
+            return 1.0D;
+        }
     }
 
     public @NotNull PrimarySkillType getLastSkillShownScoreboard() {
@@ -1059,9 +1064,12 @@ public class McMMOPlayer implements Identified {
         }
     }
 
-    private void tooTiredMultiple(PrimarySkillType primarySkillType, SubSkillType aSubSkill, SuperAbilityType aSuperAbility, SubSkillType bSubSkill, SuperAbilityType bSuperAbility) {
-        String aSuperAbilityCD = LocaleLoader.getString("Skills.TooTired.Named", aSubSkill.getLocaleName(), String.valueOf(calculateTimeRemaining(aSuperAbility)));
-        String bSuperAbilityCD = LocaleLoader.getString("Skills.TooTired.Named", bSubSkill.getLocaleName(), String.valueOf(calculateTimeRemaining(bSuperAbility)));
+    private void tooTiredMultiple(PrimarySkillType primarySkillType, SubSkillType aSubSkill,
+                                  SuperAbilityType aSuperAbility, SubSkillType bSubSkill, SuperAbilityType bSuperAbility) {
+        String aSuperAbilityCD = LocaleLoader.getString("Skills.TooTired.Named", aSubSkill.getLocaleName(),
+                String.valueOf(calculateTimeRemaining(aSuperAbility)));
+        String bSuperAbilityCD = LocaleLoader.getString("Skills.TooTired.Named", bSubSkill.getLocaleName(),
+                String.valueOf(calculateTimeRemaining(bSuperAbility)));
         String allCDStr = aSuperAbilityCD + ", " + bSuperAbilityCD;
 
         NotificationManager.sendPlayerInformation(player, NotificationType.TOOL, "Skills.TooTired.Extra",
